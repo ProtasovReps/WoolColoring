026ec82848cfc741a102ff789429564a84c23938
Konstantin Protasov <kostyapk3@gmail.com>
2025-02-02 22:24:48 +0300

    Composite root

---
+++ Assets/Source/Scripts/CompositeRoot.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class CompositeRoot : MonoBehaviour
+{
+    [SerializeField] private Picture _picture;
+    [SerializeField] private Painter _painter;
+    [SerializeField] private InputReader _inputReader;
+    [SerializeField] private WhiteStringHolder _whiteStringHolder;
+    [SerializeField] private ColoredStringHolder[] _unlockedStringHolders;
+    [SerializeField] private ColoredStringHolder[] _lockedStringHolders;
+
+    private ColoredStringHolderStash _stash;
+    private ColoredStringHolderSwitcher _switcher;
+    private StringDistributor _stringDistributor;
+
+    private void Awake()
+    {
+        _stash = new ColoredStringHolderStash(_unlockedStringHolders, _lockedStringHolders);
+        _switcher = new ColoredStringHolderSwitcher();
+        _stringDistributor = new StringDistributor(_stash, _whiteStringHolder);
+
+        _picture.Initialize();
+        _painter.Initialize(_switcher, _stash);
+
+        foreach (var holder in _unlockedStringHolders)
+        {
+            Color requiredColor = _picture.GetRequiredColor();
+
+            holder.Initialize();
+            _switcher.Switch(requiredColor, holder);
+        }
+
+        _inputReader.Initialize();
+    }
+}
