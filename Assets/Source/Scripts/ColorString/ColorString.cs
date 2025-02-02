c5294be4168dc10b415be8c124c4f11d0223b610
Konstantin Protasov <kostyapk3@gmail.com>
2025-02-02 22:19:39 +0300

    ColorString init method

--- Assets/Source/Scripts/ColorString/ColorString.cs
+++ Assets/Source/Scripts/ColorString/ColorString.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
 
+[RequireComponent (typeof(MeshRenderer))]
 public class ColorString : MonoBehaviour, IColorable
 {
-    [SerializeField] private MeshRenderer _meshRenderer;
+    private MeshRenderer _meshRenderer;
 
-    public Color Color => _meshRenderer.material.color;
+    public void Initialize() => _meshRenderer = GetComponent<MeshRenderer>();
 
-    public void SetColor(Color color)
-       => _meshRenderer.material.color = color;
+    public void SetColor(Color color) => _meshRenderer.material.color = color;
+
+    public Color GetColor() => _meshRenderer.material.color;
 }
--- Assets/Source/Scripts/Inteface/IColorable.cs
+++ Assets/Source/Scripts/Inteface/IColorable.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public interface IColorable
 {
-    Color Color { get; }
-
     void SetColor(Color color);
+
+    Color GetColor();
 }
