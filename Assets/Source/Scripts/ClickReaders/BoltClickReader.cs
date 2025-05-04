c3bca5963e0521004ece3b172704c9a691726cbf
Konstantin Protasov <kostyapk3@gmail.com>
2025-05-04 16:27:31 +0300

    Immortality when 2 colors left

--- Assets/Source/Scripts/ClickReaders/BoltClickReader.cs
+++ Assets/Source/Scripts/ClickReaders/BoltClickReader.cs
@@ -3,12 +3,18 @@ using UnityEngine;
 
 public class BoltClickReader : ClickReader
 {
+    private const int ImmortalityRequiredColorsCount = 2;
+
     private StringDistributor _distributor;
+    private WhiteStringHolder _whiteStringHolder;
+    private Picture _picture;
 
     [Inject]
-    private void Inject(StringDistributor distributor)
+    private void Inject(StringDistributor distributor, WhiteStringHolder whiteStringHolder, Picture picture)
     {
         _distributor = distributor;
+        _whiteStringHolder = whiteStringHolder;
+        _picture = picture;
     }
 
     protected override void ValidateHit(RaycastHit hit)
@@ -18,6 +24,15 @@ public class BoltClickReader : ClickReader
 
         if (bolt.IsAnimating)
             return;
+
+        if(_picture.RequiredColorsCount <= ImmortalityRequiredColorsCount)
+        {
+            if(_whiteStringHolder.StringCount >= _whiteStringHolder.MaxStringCount)
+            {
+                bolt.Unscrew();
+                return;
+            }
+        }
 
         _distributor.Distribute(bolt);
         bolt.Unscrew();
